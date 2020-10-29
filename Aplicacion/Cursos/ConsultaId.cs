9f3956b236eb6f26e97143d7c1cff217ad477935
jorge22l <jorgeluismreyes@gmail.com>
2020-10-28 21:32:55 -0600

    Agregando funcion agregar a API

---
+++ Aplicacion/Cursos/ConsultaId.cs
@@ -0,0 +1,34 @@
+﻿using Dominio;
+using MediatR;
+using Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Cursos
+{
+    public class ConsultaId
+    {
+        public class CursoUnico:IRequest<Curso>
+        {
+            public int Id { get; set; }
+        }
+
+        public class Manejador:IRequestHandler<CursoUnico, Curso>
+        {
+            private readonly CursosOnlineContext _context;
+
+            public Manejador(CursosOnlineContext context)
+            {
+                _context = context;
+            }
+            public async Task<Curso> Handle(CursoUnico request, CancellationToken cancellationToken)
+            {
+                var curso = await _context.tblCurso.FindAsync(request.Id);
+                return curso;
+            }
+        }
+    }
+}
--- ProyectoCore/Controllers/CursosController.cs
+++ ProyectoCore/Controllers/CursosController.cs
@@ -1,4 +1,6 @@
-﻿using Dominio;
+﻿using Aplicacion.Cursos;
+using Dominio;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Persistencia;
 using System;
@@ -9,18 +11,32 @@ using System.Threading.Tasks;
 namespace ProyectoCore.Controllers
 {
     [Route("api/[controller]")]
+    [ApiController]
     public class CursosController:ControllerBase
     {
-        private readonly CursosOnlineContext context;
-        public CursosController(CursosOnlineContext _context)
+        private readonly IMediator _mediator;
+
+        public CursosController(IMediator mediator)
         {
-            this.context = _context;
+            _mediator = mediator;
         }
 
         [HttpGet]
-        public IEnumerable<Curso> Get()
+        public async Task<ActionResult<List<Curso>>> Get()
         {
-            return context.tblCurso.ToList();
+            return await _mediator.Send(new Consulta.ListaCursos());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Curso>> Detalle(int id)
+        {
+            return await _mediator.Send(new ConsultaId.CursoUnico { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Crear(Nuevo.Ejecuta data)
+        {
+            return await _mediator.Send(data);
         }
     }
 }
